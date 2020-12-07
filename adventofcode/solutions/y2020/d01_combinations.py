44c6356914d0eece1dcc85d3566e562d92613dc6
codertee <75262025+codertee@users.noreply.github.com>
2020-12-08 00:18:01 +0200

    functional style rewrite

---
+++ adventofcode/solutions/y2020/d01_combinations.py
@@ -0,0 +1,33 @@
+from functools import reduce
+from itertools import combinations, dropwhile
+from operator import mul
+
+from adventofcode.inputs import get_input
+from adventofcode.utils import aoc_timer
+
+
+@aoc_timer()
+def parse_input(input_str):
+    return list(map(int, input_str.split()))
+
+
+def solve(expenses, n):
+    constraint = lambda x: sum(x) != 2020
+    accepted_values = next(dropwhile(constraint, combinations(expenses, n)))
+    return reduce(mul, accepted_values)
+
+
+@aoc_timer(1, 1, 2020)
+def solve_first(expenses):
+    return solve(expenses, 2)
+
+
+@aoc_timer(2, 1, 2020)
+def solve_second(expenses):
+    return solve(expenses, 3)
+
+
+if __name__ == '__main__':
+    expenses = parse_input(get_input(1, year=2020))
+    solve_first(expenses)
+    solve_second(expenses)
