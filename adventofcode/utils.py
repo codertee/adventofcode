faa8737f4ceab39bf5e6bac7898203dc3b09b995
codertee <75262025+codertee@users.noreply.github.com>
2020-12-07 22:11:39 +0200

    more general timer

--- adventofcode/utils.py
+++ adventofcode/utils.py
@@ -2,18 +2,27 @@ import logging
 import time
 
 
-def aoc_timer(part: int, day: int, year: int = 2020):
-    part = {1: 'one', 2: 'two'}[part]
-    identifier_str = f'{year}.{day} part {part}'
+def aoc_timer(part=0, day=0, year=0):
+    part = {1: 'one', 2: 'two'}.get(part)
+    prepend = ''
+    if year:
+        prepend += '%s.' % year
+    if day:
+        prepend += '%s ' % day
+    if part:
+        prepend += 'part %s: ' % part
     def decorator(func):
-        def wrapper(parsed_input):
+        def wrapper(*a, **kw):
             try:
                 start = time.perf_counter()
-                result = func(parsed_input)
+                result = func(*a, **kw)
                 delta = (time.perf_counter() - start) * 1000
-                print(f'{identifier_str}: {result} ({delta:.4f} ms)')
+                if not prepend:
+                    print(f'finished {func.__name__} in {delta:.4f} ms')
+                else:
+                    print(f'{prepend}{result} ({delta:.4f} ms)')
             except Exception as e:
-                logging.exception(f'exception when solving {identifier_str}: {e}')
+                logging.exception(f'exception when solving {prepend}: {e}')
             else:
                 return result
         return wrapper
