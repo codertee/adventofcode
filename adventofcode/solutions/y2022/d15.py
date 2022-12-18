36e0bf55513626eb07107faeff5bfa3d90c0f0f8
codertee <75262025+codertee@users.noreply.github.com>
2022-12-18 03:13:31 +0200

    faster single core solution

--- adventofcode/solutions/y2022/d15.py
+++ adventofcode/solutions/y2022/d15.py
@@ -1,11 +1,13 @@
 import re
-from functools import partial
+from collections import namedtuple
 from itertools import combinations
 from math import inf
-from multiprocessing import Pool
 
 from adventofcode.inputs import get_input
 from adventofcode.utils import aoc_timer
+
+
+Sensor = namedtuple('Sensor', ['x', 'y', 'radius'])
 
 
 def parse_input(input_str):
@@ -13,7 +15,7 @@ def parse_input(input_str):
     for line in input_str.splitlines():
         x, y, bx, by = map(int, re.findall(r'(-?\d+)', line))
         r = abs(x - bx) + abs(y - by)
-        sensors.append((x, y, r))
+        sensors.append(Sensor(x, y, r))
     return sensors
 
 
@@ -31,41 +33,34 @@ def solve_first(sensors):
     return max_x - min_x
 
 
-def solve(sensors, y):
-    ranges = []
-    for sx, sy, r in sensors:
-        dy = abs(y - sy)
-        if dy > r:
-            continue
-        dx = r - dy
-        ranges.append((sx - dx, sx + dx))
-    if len(ranges) < 4:
-        return
-    ranges.sort()
-    prev_x2 = ranges[0][1]
-    for x1, x2 in ranges[1:]:
-        if x1 > prev_x2:
-            return prev_x2 + 1, y
-        prev_x2 = max(x2, prev_x2)
-    return False
+def sign(a, b):
+    return (a > b) - (a < b)
+
+
+def distance(one, other):
+    return abs(one.x - other.x) + abs(one.y - other.y)
 
 
 @aoc_timer(2, 15, 2022)
 def solve_second(sensors):
-    filtered_sensors = []
+    one_away = []
     for one, other in combinations(sensors, 2):
-        (x1, y1, r1), (x2, y2, r2) = one, other
-        if abs(x1 - x2) + abs(y1 - y2) == r1 + r2 + 2:
-            filtered_sensors.extend([one, other])
-    filtered_sensors.sort(key=lambda x: x[2])
-    min_y, max_y = 4_000_000, 0
-    for _, y, _ in sensors:
-        min_y = min(min_y, y)
-        max_y = max(max_y, y)
-    with Pool() as p:
-        results = p.map(partial(solve, filtered_sensors), range(min_y, max_y))
-        x, y = next(filter(bool, results))
-        return x * 4_000_000 + y
+        if distance(one, other) == one.radius + other.radius + 2:
+            one_away += [one, other]
+    (x1, y1, r1), (x2, y2, _) = one_away[:2]
+    dx = sign(x1, x2)
+    dy = -sign(y1, y2)
+    x, y = x1 - dx * (r1 + 1), y1
+    for _ in range(r1 + 2):
+        test = Sensor(x, y, 0)
+        if all(
+            distance(test, other) == other.radius + 1 
+            for other in one_away
+        ):
+            return x * 4_000_000 + y
+        x += dx
+        y += dy
+    return "edge cases not handled"
 
 
 if __name__ == '__main__':
